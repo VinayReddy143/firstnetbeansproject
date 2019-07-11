877be91e6b2fb137cf6463ced42171fa2d0a6059
1992856 <1992856@E20734.CEGEPGIM.LCL>
2019-07-11 14:41:19 -0400

    first commit in netbeans

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.time.Clock;
+
+/**
+ *
+ * @author 1992856
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
